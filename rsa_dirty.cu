17f793e04c153f9cf5a71822d8d0832ee28a6b03
TBoonX <tboonx@googlemail.com>
2013-01-22 20:25:10 +0100

    Update rsa_dirty.cu
    neue Schlüssel und einfaches (un)splitt

--- rsa_dirty.cu
+++ rsa_dirty.cu
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 //Variablen
-#define p 3
-#define q 5
-#define n 15
-#define e 3
-#define v 3
-#define z 8
+#define p 5
+#define q 7
+#define n 35
+#define e 5
+#define v 5
+#define z 24
 #define anzahl_Zeichen 2688
 #define count_cores 384
+
 /*
 Klartext: K
 Geheimtext: G
@@ -16,23 +18,26 @@ Verschluesselung: G = K^v mod n
 Entschluesselung: K = G^e mod n
 Index des CUDA Kerns: blockIdx.x blockIdx.y
 Ein groesserer Text soll ver- und entschluesselt werden.
-Dieser wird jedoch wie folgt veraendert: nur kleine Buchstaben, keine Sonderzeichen auﬂer . und ,
+Dieser wird jedoch wie folgt veraendert: nur kleine Buchstaben, keine Sonderzeichen au√üer . und ,
 Dafuer werden die einzellnen chars in Integer umgewandelt (<=28) und dies halbiert und auf 3 Werte verteilt.
 Somit ist eine Verarbeitung moeglich.
 */
+
 static void HandleError( cudaError_t err, const char *file, int line ) {
 	if (err != cudaSuccess) {
 		printf( "%s in %s at line %d\n", cudaGetErrorString( err ), file, line );
 		exit( EXIT_FAILURE );
 	}
 }
+
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
 __global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 {
 	long int i, j, multi, x;
 	
 	/*
-	 * Schleife f¸r Blockgroesse 384 mit je einem Thread
+	 * Schleife f√ºr Blockgroesse 384 mit je einem Thread
 	
 	long int block_length = anzahl_Zeichen/count_cores;
 	
@@ -49,7 +54,6 @@ __global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 	
 	//Fuer 384 Bloecke mit 7 Threads
 	long int threads = anzahl_Zeichen/count_cores;
-	threads *= 3;	
 
 	multi = x  = klartexte[threadIdx.x+blockIdx.x*threads];
 	for (j = 1; j < v; j++)
@@ -57,12 +61,13 @@ __global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 	
 	geheimtexte[threadIdx.x+blockIdx.x*threads] = x % n;
 }
+
 __global__ void entschluessselung(long int geheimtexte[], long int klartexte_pruefung[])
 {
 	long int i, j, multi, x;
 	
 	/*
-	 * Schleife f¸r Blockgroesse 384 mit je einem Thread
+	 * Schleife f√ºr Blockgroesse 384 mit je einem Thread
 	
 	long int block_length = anzahl_Zeichen/count_cores;
 	for (i = 0 ; i < block_length; i++)
@@ -77,7 +82,6 @@ __global__ void entschluessselung(long int geheimtexte[], long int klartexte_pru
 	
 	//Fuer 384 Bloecke mit 7 Threads
 	long int threads = anzahl_Zeichen/count_cores;
-	threads *= 3;	
 	
 	multi = x  = geheimtexte[threadIdx.x+blockIdx.x*threads];
 	for (j = 1; j < e; j++)
@@ -85,6 +89,7 @@ __global__ void entschluessselung(long int geheimtexte[], long int klartexte_pru
 	
 	klartexte_pruefung[threadIdx.x+blockIdx.x*threads] = x % n;
 }
+
 void splitt(char text[], long int numbers[])
 {
 	int i;
@@ -93,7 +98,6 @@ void splitt(char text[], long int numbers[])
 	for (i = 0; i < anzahl_Zeichen; i++)
 	{
 		long int number = (int)text[i];
-		int modulo, multi;
 		
 		//char in int beginnend mit 0
 		//Sonderzeichen
@@ -108,30 +112,10 @@ void splitt(char text[], long int numbers[])
 		else				//a-z
 			number -= 97;
 		
-		modulo = number % 10;
-		multi = (int)(number/10);
-		
-		//splitt
-		if (multi == 0)
-		{
-			numbers[i*3] = modulo;
-			numbers[i*3+1] = 0;
-			numbers[i*3+2] = 0;
-		}
-		else if (multi == 1)
-		{
-			numbers[i*3] = 10;
-			numbers[i*3+1] = modulo;
-			numbers[i*3+2] = 0;
-		}
-		else
-		{
-			numbers[i*3] = 10;
-			numbers[i*3+1] = 10;
-			numbers[i*3+2] = number-20;
-		}
+		numbers[i] = number;
 	}
 }
+
 void unsplitt(char text[], long int numbers[])
 {
 	int i;
@@ -139,7 +123,7 @@ void unsplitt(char text[], long int numbers[])
 	//Splitte Klartext
 	for (i = 0; i < anzahl_Zeichen; i++)
 	{
-		long int number = numbers[i*3]+numbers[i*3+1]+numbers[i*3+2];
+		long int number = numbers[i];
 		char t;
 		
 		//int in char
@@ -158,6 +142,7 @@ void unsplitt(char text[], long int numbers[])
 		text[i] = t;
 	}
 }
+
 int main(void) {
 	int i;
 	cudaEvent_t start, stop;
@@ -166,21 +151,22 @@ int main(void) {
 	
 	char klartext[anzahl_Zeichen+1];
 	char klartext2[anzahl_Zeichen+1];
-	long int kt_splitted[anzahl_Zeichen*3+1];
-	long int kt_splitted2[anzahl_Zeichen*3+1];
+	long int kt_splitted[anzahl_Zeichen+1];
+	long int kt_splitted2[anzahl_Zeichen+1];
 	long int *dev_kt_splitted, *dev_kt_splitted2, *dev_gt_splitted;
-	int size = sizeof(long int)*(anzahl_Zeichen*3+1);
+	int size = sizeof(long int)*(anzahl_Zeichen+1);
 	
 	//Debug
 	printf("\na: %d   z: %d   ,: %d   .: %d   ?: %d    : %d   backn: %d\n\n", (int)'a', (int)'z', (int)',', (int)'.', (int)'?', (int)' ',  (int)'\n');
 	
-	//TESt
+	//TEST
 	splitt(klartext, kt_splitted);
 	for (i = 0; i < anzahl_Zeichen; i++)
 	{
 		klartext[i] = '-';
 	}
 	unsplitt(klartext, kt_splitted);
+	
 	//Klartetext erzeugen
 	klartext[anzahl_Zeichen] = klartext2[anzahl_Zeichen] = '\0';
 	strcpy(klartext, "hat der alte hexenmeister?sich doch einmal wegbegeben.?und nun sollen seine geister?auch nach meinem willen leben.?seine wort und werke?merkt ich und den brauch,?und mit geistesstaerke?tu ich wunder auch.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?und nun komm, du alter besen.?nimm die schlechten lumpenhuellen ?bist schon lange knecht gewesen:?nun erfuelle meinen willen.?auf zwei beinen stehe,?oben sei ein kopf,?eile nun und gehe?mit dem wassertopf.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?seht, er laeuft zum ufer nieder,?wahrlich. ist schon an dem flusse,?und mit blitzesschnelle wieder?ist er hier mit raschem gusse.?schon zum zweiten male.?wie das becken schwillt.?wie sich jede schale?voll mit wasser fuellt.?stehe. stehe.?denn wir haben?deiner gaben?vollgemessen. ?ach, ich merk es. wehe. wehe.?hab ich doch das wort vergessen.?ach, das wort, worauf am ende?er das wird, was er gewesen.?ach, er laeuft und bringt behende.?waerst du doch der alte besen.?immer neue guesse?bringt er schnell herein,?ach. und hundert fluesse?stuerzen auf mich ein.?nein, nicht laenger?kann ichs lassen ?will ihn fassen.?das ist tuecke.?ach. nun wird mir immer baenger.?welche mine. welche blicke.?o du ausgeburt der hoelle.?soll das ganze haus ersaufen??seh ich ueber jede schwelle?doch schon wasserstroeme laufen.?ein verruchter besen,?der nicht hoeren will.?stock, der du gewesen,?steh doch wieder still.?willst am ende?gar nicht lassen??will dich fassen,?will dich halten?und das alte holz behende?mit dem scharfen beile spalten.?seht da kommt er schleppend wieder.?wie ich mich nur auf dich werfe,?gleich, o kobold, liegst du nieder ?krachend trifft die glatte schaerfe.?wahrlich, brav getroffen.?seht, er ist entzwei.?und nun kann ich hoffen,?und ich atme frei.?wehe. wehe.?beide teile?stehn in eile?schon als knechte?voellig fertig in die hoehe.?helft mir, ach. ihr hohen maechte.?und sie laufen. nass und naesser?wirds im saal und auf den stufen.?welch entsetzliches gewaesser.?herr und meister. hoer mich rufen.  ?ach, da kommt der meister.?herr, die not ist gross.?die ich rief, die geister?werd ich nun nicht los.?in die ecke,?besen, besen.?seids gewesen.?denn als geister?ruft euch nur zu diesem zwecke,?erst hervor der alte meister.?                                                                                                                                                                                                                                                                                                    ");
@@ -196,7 +182,7 @@ int main(void) {
 	}
 	printf("\n\n");
 	
-	//klartext2 mit a f¸llen
+	//klartext2 mit a f√ºllen
 	for (i = 0; i < anzahl_Zeichen; i++)
 	{
 		klartext2[i] = 'a';
@@ -204,25 +190,31 @@ int main(void) {
 	
 	//Chars in ints aufsplitten
 	splitt(klartext, kt_splitted);
+	
 	//Variablen der Zeitmessung erstellen
 	HANDLE_ERROR(cudaEventCreate(&start));
 	HANDLE_ERROR(cudaEventCreate(&stop));
+	
 	//Start Zeitmessung
 	HANDLE_ERROR(cudaEventRecord(start, 0));
+	
 	//allokieren
-	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted, size*10));
+	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted, size));
 	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted2, size*10));
 	HANDLE_ERROR(cudaMalloc((void **)&dev_gt_splitted, size*10));
+	
 	//kopieren
 	HANDLE_ERROR(cudaMemcpy(dev_kt_splitted, kt_splitted, size, cudaMemcpyHostToDevice));
+	
 	//Block festlegen
 	//dim3 blocks(count_cores, 1);
 	
 	//Anzahl Threads pro Block
 	count_Threads = anzahl_Zeichen/count_cores;
-	count_Threads *= 3; 
+	
 	//verschluesseln
 	verschluessselung<<<count_cores, count_Threads>>>(dev_kt_splitted, dev_gt_splitted);
+	
 	//zurueckkopieren
 	//HANDLE_ERROR(cudaMemcpy(geheimtexte, dev_geheimtexte, sizeof(geheimtexte), cudaMemcpyDeviceToHost));
 	
@@ -238,6 +230,7 @@ int main(void) {
 	//Ende der Zeitmessung
 	HANDLE_ERROR(cudaEventRecord(stop, 0));
 	HANDLE_ERROR(cudaEventSynchronize(stop));
+	
 	//Ausgabe der verstrichenen Zeit
 	HANDLE_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
 	printf("Elapsed time: %3.1f ms\n", elapsedTime);
@@ -252,6 +245,7 @@ int main(void) {
 		putchar(klartext2[i]);
 	}
 	printf("\n\n");
+	
 	//freigeben
 	HANDLE_ERROR(cudaEventDestroy(start));
 	HANDLE_ERROR(cudaEventDestroy(stop));
