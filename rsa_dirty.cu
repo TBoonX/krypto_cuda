bf73ea97af7864540ca3cdef7a3d4883f48311b5
TBoonX <tboonx@googlemail.com>
2013-01-22 20:39:24 +0100

    Update rsa_dirty.cu
    Kommentare eingefügt und aufgeräumt

--- rsa_dirty.cu
+++ rsa_dirty.cu
@@ -19,7 +19,7 @@ Entschluesselung: K = G^e mod n
 Index des CUDA Kerns: blockIdx.x blockIdx.y
 Ein groesserer Text soll ver- und entschluesselt werden.
 Dieser wird jedoch wie folgt veraendert: nur kleine Buchstaben, keine Sonderzeichen außer . und ,
-Dafuer werden die einzellnen chars in Integer umgewandelt (<=28) und dies halbiert und auf 3 Werte verteilt.
+Dafuer werden die einzellnen chars in Integer umgewandelt (<=30).
 Somit ist eine Verarbeitung moeglich.
 */
 
@@ -34,23 +34,7 @@ static void HandleError( cudaError_t err, const char *file, int line ) {
 
 __global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 {
-	long int i, j, multi, x;
-	
-	/*
-	 * Schleife für Blockgroesse 384 mit je einem Thread
-	
-	long int block_length = anzahl_Zeichen/count_cores;
-	
-	for (i = 0 ; i < block_length; i++)
-	{
-		multi = x  = klartexte[i+blockIdx.x*count_cores];
-		for (j = 1; j < v; j++)
-			x *= multi;
-		
-		geheimtexte[i+blockIdx.x*count_cores] = x % n;
-	}
-	*/
-	
+	long int j, multi, x;
 	
 	//Fuer 384 Bloecke mit 7 Threads
 	long int threads = anzahl_Zeichen/count_cores;
@@ -64,21 +48,7 @@ __global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 
 __global__ void entschluessselung(long int geheimtexte[], long int klartexte_pruefung[])
 {
-	long int i, j, multi, x;
-	
-	/*
-	 * Schleife für Blockgroesse 384 mit je einem Thread
-	
-	long int block_length = anzahl_Zeichen/count_cores;
-	for (i = 0 ; i < block_length; i++)
-	{
-		multi = x  = geheimtexte[i+blockIdx.x*count_cores];
-		for (j = 1; j < e; j++)
-			x *= multi;
-		
-		klartexte_pruefung[i+blockIdx.x*count_cores] = x % n;
-	}
-	*/
+	long int j, multi, x;
 	
 	//Fuer 384 Bloecke mit 7 Threads
 	long int threads = anzahl_Zeichen/count_cores;
@@ -156,16 +126,7 @@ int main(void) {
 	long int *dev_kt_splitted, *dev_kt_splitted2, *dev_gt_splitted;
 	int size = sizeof(long int)*(anzahl_Zeichen+1);
 	
-	//Debug
-	printf("\na: %d   z: %d   ,: %d   .: %d   ?: %d    : %d   backn: %d\n\n", (int)'a', (int)'z', (int)',', (int)'.', (int)'?', (int)' ',  (int)'\n');
-	
-	//TEST
-	splitt(klartext, kt_splitted);
-	for (i = 0; i < anzahl_Zeichen; i++)
-	{
-		klartext[i] = '-';
-	}
-	unsplitt(klartext, kt_splitted);
+	printf("\n-|| RSA mit CUDA ||-\n\n\n");
 	
 	//Klartetext erzeugen
 	klartext[anzahl_Zeichen] = klartext2[anzahl_Zeichen] = '\0';
@@ -173,12 +134,13 @@ int main(void) {
 	printf("\n\nDer Klartext ist %d Zeichen lang.\n", sizeof(klartext)/sizeof(char));
 	
 	//Ausgabe
-	printf("\n\nDer Klartext:\n");
+	printf("\n\nAnfang des  Klartextes:\n\n");
 	for (i = 0; i < anzahl_Zeichen; i++)
 	{
 		if (klartext[i] == '?')
 			klartext[i] = '\n';
-		putchar(klartext[i]);
+		if (i < 546)
+			putchar(klartext[i]);
 	}
 	printf("\n\n");
 	
@@ -187,6 +149,8 @@ int main(void) {
 	{
 		klartext2[i] = 'a';
 	}
+	
+	printf("Der Klartext wird nun verschluesselt und anschliessend entschluesselt.\n");
 	
 	//Chars in ints aufsplitten
 	splitt(klartext, kt_splitted);
@@ -200,23 +164,17 @@ int main(void) {
 	
 	//allokieren
 	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted, size));
-	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted2, size*10));
-	HANDLE_ERROR(cudaMalloc((void **)&dev_gt_splitted, size*10));
+	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted2, size));
+	HANDLE_ERROR(cudaMalloc((void **)&dev_gt_splitted, size));
 	
 	//kopieren
 	HANDLE_ERROR(cudaMemcpy(dev_kt_splitted, kt_splitted, size, cudaMemcpyHostToDevice));
-	
-	//Block festlegen
-	//dim3 blocks(count_cores, 1);
 	
 	//Anzahl Threads pro Block
 	count_Threads = anzahl_Zeichen/count_cores;
 	
 	//verschluesseln
 	verschluessselung<<<count_cores, count_Threads>>>(dev_kt_splitted, dev_gt_splitted);
-	
-	//zurueckkopieren
-	//HANDLE_ERROR(cudaMemcpy(geheimtexte, dev_geheimtexte, sizeof(geheimtexte), cudaMemcpyDeviceToHost));
 	
 	//sync
 	HANDLE_ERROR(cudaDeviceSynchronize());
@@ -233,14 +191,14 @@ int main(void) {
 	
 	//Ausgabe der verstrichenen Zeit
 	HANDLE_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
-	printf("Elapsed time: %3.1f ms\n", elapsedTime);
+	printf("\nBeendet.\n\n\nverstrichene Zeit: %3.1f ms\n", elapsedTime);
 	
 	//ints wieder in char umwandeln
 	unsplitt(klartext2, kt_splitted2);
 	
 	//Ausgabe
-	printf("\n\nDer Klartext lautet nun:\n");
-	for (i = 0; i < anzahl_Zeichen; i++)
+	printf("\n\nDer Klartext lautet nun: (Anfang)\n\n");
+	for (i = 0; i < 545; i++)
 	{
 		putchar(klartext2[i]);
 	}
@@ -254,4 +212,3 @@ int main(void) {
 	HANDLE_ERROR(cudaFree(dev_gt_splitted));
 	return EXIT_SUCCESS;
 }
-
