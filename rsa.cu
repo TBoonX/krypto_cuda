df19aee82f9c9ffbca0bc6440b6c373eee0d30a4
TBoonX <tboonx@googlemail.com>
2013-01-23 21:42:19 +0100

    Update rsa.cu
    Mit Stand vom 22.01.2013 parameter von multi eingebaut.
    multi mal wird verschlüsselt nd entschlüsselt

--- rsa.cu
+++ rsa.cu
@@ -1,10 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define output 0
-
-#define zauberlehrling 2688
-
 //Variablen
 #define p 5
 #define q 7
@@ -12,8 +8,7 @@
 #define e 5
 #define v 5
 #define z 24
-//#define anzahl_Zeichen 268800
-long int anzahl_Zeichen = 0;
+#define anzahl_Zeichen 2688
 #define count_cores 384
 
 /*
@@ -37,7 +32,7 @@ static void HandleError( cudaError_t err, const char *file, int line ) {
 
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
-__global__ void verschluessselung(long int klartexte[], long int geheimtexte[], long int anzahl_Zeichen)
+__global__ void verschluessselung(long int klartexte[], long int geheimtexte[])
 {
 	long int j, multi, x;
 	
@@ -51,7 +46,7 @@ __global__ void verschluessselung(long int klartexte[], long int geheimtexte[], 
 	geheimtexte[threadIdx.x+blockIdx.x*threads] = x % n;
 }
 
-__global__ void entschluessselung(long int geheimtexte[], long int klartexte_pruefung[], long int anzahl_Zeichen)
+__global__ void entschluessselung(long int geheimtexte[], long int klartexte_pruefung[])
 {
 	long int j, multi, x;
 	
@@ -129,48 +124,43 @@ int main(int argc, char *argv[]) {
 	char klartext2[anzahl_Zeichen+1];
 	long int kt_splitted[anzahl_Zeichen+1];
 	long int kt_splitted2[anzahl_Zeichen+1];
-	long int *dev_kt_splitted, *dev_kt_splitted2, *dev_gt_splitted, *dev_anzahl_Zeichen;
+	long int *dev_kt_splitted, *dev_kt_splitted2, *dev_gt_splitted;
 	int size = sizeof(long int)*(anzahl_Zeichen+1);
+	
+	printf("\n-|| RSA mit CUDA ||-\n\n\n");
+	
+	//Klartetext erzeugen
+	klartext[anzahl_Zeichen] = klartext2[anzahl_Zeichen] = '\0';
+	strcpy(klartext, "hat der alte hexenmeister?sich doch einmal wegbegeben.?und nun sollen seine geister?auch nach meinem willen leben.?seine wort und werke?merkt ich und den brauch,?und mit geistesstaerke?tu ich wunder auch.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?und nun komm, du alter besen.?nimm die schlechten lumpenhuellen ?bist schon lange knecht gewesen:?nun erfuelle meinen willen.?auf zwei beinen stehe,?oben sei ein kopf,?eile nun und gehe?mit dem wassertopf.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?seht, er laeuft zum ufer nieder,?wahrlich. ist schon an dem flusse,?und mit blitzesschnelle wieder?ist er hier mit raschem gusse.?schon zum zweiten male.?wie das becken schwillt.?wie sich jede schale?voll mit wasser fuellt.?stehe. stehe.?denn wir haben?deiner gaben?vollgemessen. ?ach, ich merk es. wehe. wehe.?hab ich doch das wort vergessen.?ach, das wort, worauf am ende?er das wird, was er gewesen.?ach, er laeuft und bringt behende.?waerst du doch der alte besen.?immer neue guesse?bringt er schnell herein,?ach. und hundert fluesse?stuerzen auf mich ein.?nein, nicht laenger?kann ichs lassen ?will ihn fassen.?das ist tuecke.?ach. nun wird mir immer baenger.?welche mine. welche blicke.?o du ausgeburt der hoelle.?soll das ganze haus ersaufen??seh ich ueber jede schwelle?doch schon wasserstroeme laufen.?ein verruchter besen,?der nicht hoeren will.?stock, der du gewesen,?steh doch wieder still.?willst am ende?gar nicht lassen??will dich fassen,?will dich halten?und das alte holz behende?mit dem scharfen beile spalten.?seht da kommt er schleppend wieder.?wie ich mich nur auf dich werfe,?gleich, o kobold, liegst du nieder ?krachend trifft die glatte schaerfe.?wahrlich, brav getroffen.?seht, er ist entzwei.?und nun kann ich hoffen,?und ich atme frei.?wehe. wehe.?beide teile?stehn in eile?schon als knechte?voellig fertig in die hoehe.?helft mir, ach. ihr hohen maechte.?und sie laufen. nass und naesser?wirds im saal und auf den stufen.?welch entsetzliches gewaesser.?herr und meister. hoer mich rufen.  ?ach, da kommt der meister.?herr, die not ist gross.?die ich rief, die geister?werd ich nun nicht los.?in die ecke,?besen, besen.?seids gewesen.?denn als geister?ruft euch nur zu diesem zwecke,?erst hervor der alte meister.?                                                                                                                                                                                                                                                                                                    ");
+	printf("\n\nDer Klartext ist %d Zeichen lang.\n", sizeof(klartext)/sizeof(char));
 	
 	if (argc < 2)
 	{
 		printf("\nParameter fuer Groesse der Klartexted fehlt!\n");
 		exit(0);
 	}
-	
+
 	//lese Anzahl der Zeichen
 	multi = atoi(argv[1]);
-	
+
 	if (multi < 1)
 	{
 		printf("\nAnzahl der Zeichen ist eine ganze positive Zahl.\n");
 		exit(0);
 	}
 	
-	anzahl_Zeichen = multi*zauberlehrling;
-	
-	printf("\n-|| RSA mit CUDA ||-\n\n\n");
-	
-	//Klartetext erzeugen
-	klartext[anzahl_Zeichen] = klartext2[anzahl_Zeichen] = '\0';
-	for (i = 0; i < multi; i++)
-		strcpy(&klartext[i*zauberlehrling], "hat der alte hexenmeister?sich doch einmal wegbegeben.?und nun sollen seine geister?auch nach meinem willen leben.?seine wort und werke?merkt ich und den brauch,?und mit geistesstaerke?tu ich wunder auch.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?und nun komm, du alter besen.?nimm die schlechten lumpenhuellen ?bist schon lange knecht gewesen:?nun erfuelle meinen willen.?auf zwei beinen stehe,?oben sei ein kopf,?eile nun und gehe?mit dem wassertopf.?walle. walle?manche strecke,?dass, zum zwecke,?wasser fliesse?und mit reichem, vollem schwalle?zu dem bade sich ergiesse.?seht, er laeuft zum ufer nieder,?wahrlich. ist schon an dem flusse,?und mit blitzesschnelle wieder?ist er hier mit raschem gusse.?schon zum zweiten male.?wie das becken schwillt.?wie sich jede schale?voll mit wasser fuellt.?stehe. stehe.?denn wir haben?deiner gaben?vollgemessen. ?ach, ich merk es. wehe. wehe.?hab ich doch das wort vergessen.?ach, das wort, worauf am ende?er das wird, was er gewesen.?ach, er laeuft und bringt behende.?waerst du doch der alte besen.?immer neue guesse?bringt er schnell herein,?ach. und hundert fluesse?stuerzen auf mich ein.?nein, nicht laenger?kann ichs lassen ?will ihn fassen.?das ist tuecke.?ach. nun wird mir immer baenger.?welche mine. welche blicke.?o du ausgeburt der hoelle.?soll das ganze haus ersaufen??seh ich ueber jede schwelle?doch schon wasserstroeme laufen.?ein verruchter besen,?der nicht hoeren will.?stock, der du gewesen,?steh doch wieder still.?willst am ende?gar nicht lassen??will dich fassen,?will dich halten?und das alte holz behende?mit dem scharfen beile spalten.?seht da kommt er schleppend wieder.?wie ich mich nur auf dich werfe,?gleich, o kobold, liegst du nieder ?krachend trifft die glatte schaerfe.?wahrlich, brav getroffen.?seht, er ist entzwei.?und nun kann ich hoffen,?und ich atme frei.?wehe. wehe.?beide teile?stehn in eile?schon als knechte?voellig fertig in die hoehe.?helft mir, ach. ihr hohen maechte.?und sie laufen. nass und naesser?wirds im saal und auf den stufen.?welch entsetzliches gewaesser.?herr und meister. hoer mich rufen.  ?ach, da kommt der meister.?herr, die not ist gross.?die ich rief, die geister?werd ich nun nicht los.?in die ecke,?besen, besen.?seids gewesen.?denn als geister?ruft euch nur zu diesem zwecke,?erst hervor der alte meister.?                                                                                                                                                                                                                                                                                                    ");
-	
-	printf("\n\nDer Klartext ist %d Zeichen lang.\n", sizeof(klartext)/sizeof(char) -1);
+	printf("\nmulti: %d\n");
 	
 	//Ausgabe
-	if (output) 
+	printf("\n\nAnfang des  Klartextes:\n\n");
+	for (i = 0; i < anzahl_Zeichen; i++)
 	{
-		printf("\n\nAnfang des  Klartextes:\n\n");
-		for (i = 0; i < anzahl_Zeichen; i++)
-		{
-			if (klartext[i] == '?')
-				klartext[i] = '\n';
-			if (i < 546)
-				putchar(klartext[i]);
-		}
-		printf("\n\n");
+		if (klartext[i] == '?')
+			klartext[i] = '\n';
+		if (i < 546)
+			putchar(klartext[i]);
 	}
+	printf("\n\n");
 	
 	//klartext2 mit a füllen
 	for (i = 0; i < anzahl_Zeichen; i++)
@@ -194,23 +184,27 @@ int main(int argc, char *argv[]) {
 	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted, size));
 	HANDLE_ERROR(cudaMalloc((void **)&dev_kt_splitted2, size));
 	HANDLE_ERROR(cudaMalloc((void **)&dev_gt_splitted, size));
-	HANDLE_ERROR(cudaMalloc((void **)&dev_anzahl_Zeichen, sizeof(anzahl_Zeichen)));
 	
 	//kopieren
 	HANDLE_ERROR(cudaMemcpy(dev_kt_splitted, kt_splitted, size, cudaMemcpyHostToDevice));
-	HANDLE_ERROR(cudaMemcpy(dev_anzahl_Zeichen, anzahl_Zeichen, sizeof(anzahl_Zeichen), cudaMemcpyHostToDevice));
 	
 	//Anzahl Threads pro Block
 	count_Threads = anzahl_Zeichen/count_cores;
 	
-	//verschluesseln
-	verschluessselung<<<count_cores, count_Threads>>(dev_kt_splitted, dev_gt_splitted, dev_anzahl_Zeichen);
-	
-	//sync
-	HANDLE_ERROR(cudaDeviceSynchronize());
-	
-	//entschluesseln
-	entschluessselung<<<count_cores, count_Threads>>>(dev_gt_splitted, dev_kt_splitted2, dev_anzahl_Zeichen);
+	for (i = 0; i < multi; i++)
+	{
+		//verschluesseln
+		verschluessselung<<<count_cores, count_Threads>>>(dev_kt_splitted, dev_gt_splitted);
+		
+		//sync
+		HANDLE_ERROR(cudaDeviceSynchronize());
+		
+		//entschluesseln
+		entschluessselung<<<count_cores, count_Threads>>>(dev_gt_splitted, dev_kt_splitted2);
+		
+		//sync
+		HANDLE_ERROR(cudaDeviceSynchronize());
+	}
 	
 	//zurueckkopieren
 	HANDLE_ERROR(cudaMemcpy(kt_splitted2, dev_kt_splitted2, size, cudaMemcpyDeviceToHost));
@@ -227,15 +221,12 @@ int main(int argc, char *argv[]) {
 	unsplitt(klartext2, kt_splitted2);
 	
 	//Ausgabe
-	if (output)
+	printf("\n\nDer Klartext lautet nun: (Anfang)\n\n");
+	for (i = 0; i < 545; i++)
 	{
-		printf("\n\nDer Klartext lautet nun: (Anfang)\n\n");
-		for (i = 0; i < 545; i++)
-		{
-			putchar(klartext2[i]);
-		}
-		printf("\n\n");
+		putchar(klartext2[i]);
 	}
+	printf("\n\n");
 	
 	//freigeben
 	HANDLE_ERROR(cudaEventDestroy(start));
