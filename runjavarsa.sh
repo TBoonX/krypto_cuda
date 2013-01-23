e3f30eaed8277bfa149da32b74a81fab7792b47b
mkirbst <m@mkirbst.de>
2013-01-23 22:52:20 +0100

    excel ueberarbeitet (268801 -> 2688)

--- runjavarsa.sh
+++ runjavarsa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##for i in 01 02 03 04 05 06 07 08 09 {10..11}
-for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096
+for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
 do
 	echo "[n $i ] `java -jar rsa_java.jar -f klartext -n $i`"
 done
